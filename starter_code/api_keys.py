11c328c534fc12d8590e744cfc6ec5ea14651292
SamuelTHaynes <128173955+SamuelTHaynes@users.noreply.github.com>
2023-09-05 00:08:02 -0500

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "d1b8fcce396f82822f429ff7b952327c"
+weather_api_key =
 
 # Geoapify API Key
-geoapify_key = "75aa7f36c88740aa9d8960aa12585bbc"
+geoapify_key = 
